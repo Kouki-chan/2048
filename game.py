bdb88a5e0b636f0a1ce152265d0a216c23db5f87
Kouki <kevinc2023@gmail.com>
2022-01-03 10:45:52 -0500

    transpose

--- game.py
+++ game.py
@@ -14,6 +14,10 @@ class Game(tk.Frame):
         self.main_grid.grid(pady=(100,0))
         self.gui()
         self.start_game()
+        self.master.bind("<Left>", self.left)
+        self.master.bind("<Right>", self.right)
+        self.master.bind("<Up>", self.up)
+        self.master.bind("<Down>", self.down)
         self.mainloop()
     
     def gui(self):
@@ -58,8 +62,8 @@ class Game(tk.Frame):
             text = "2"
         ) 
         while(self.matrix[row][col] != 0):
-            row = random.ranint(0,3)
-            col = random.ranint(0,3)
+            row = random.randint(0,3)
+            col = random.randint(0,3)
         self.matrix[row][col] = 2
         self.cells[row][col]["frame"].configure(bg=c.cell_colors[2])
         self.cells[row][col]["number"].configure(
@@ -70,5 +74,121 @@ class Game(tk.Frame):
         ) 
 
         self.score = 0
+
+    def stack(self):
+        new_matrix = [[0] * 4 for _ in range(4)]
+        for i in range(4):
+            fill_pot = 0
+            for j in range(4):
+                if self.matrix[i][j] != 0:
+                    new_matrix[i][fill_pot] = self.matrix[i][j]
+                    fill_pot += 1
+        self.matrix = new_matrix
+
+    def combine(self):
+        for i in range(4):
+            for j in range(3):
+                if self.matrix[i][j] != 0 and self.matrix == self.matrix[i][j + 1]:
+                    self.matrix[i][j] *= 2
+                    self.martix[i][j + 1] = 0
+                    self.score += self.matrix[i][j]
+
+    def reverse(self):
+        new_matrix = [] 
+        for i in range(4):
+            new_matrix.append([])
+            for j in range(4):
+                new_matrix[i].append(self.matrix[i][3 - j])
+        self.matrix = new_matrix
+    
+    def transpose(self):
+        new_matrix = [[0] * 4 for _ in range(4)]
+        for i in range(4):
+            for j in range(4):
+                new_matrix[i][j] = self.matrix[j][i]
+        self.matrix = new_matrix
+
+    def add_new_tile(self):
+        row = random.randint(0,3)
+        col = random.randint(0,3)
+        while(self.matrix[row][col] != 0):
+            row = random.randint(0,3)
+            col = random.randint(0,3)
+        self.matrix[row][col] = random.choice([2,4])
+
+    def update_gui(self):
+        for i in range(4):
+            for j in range(4):
+                cell_vaule = self.matrix[i][j]
+                if cell_vaule == 0:
+                    self.cells[i][j]["frame"].configure(bg=c.empty_cell_color)
+                    self.cells[i][j]["number"].configure(bg=c.empty_cell_color, text="")
+                else:
+                    self.cells[i][j]['frame'].configure(bg=c.cell_colors[cell_vaule])
+                    self.cells[i][j]['text'].configure(
+                        bg=c.cell_colors[cell_vaule],
+                        fg=c.cell_number_color[cell_vaule],
+                        font=c.cell_number_font[cell_vaule],
+                        text=str(cell_vaule)
+                        )
+        self.score_label.configure(text=self.score)
+        self.update_idletasks()
+    
+    def left(self, event):
+        self.stack()
+        self.combine()
+        self.stack()
+        self.add_new_tile()
+        self.update_gui()
+
+    def right(self, event):
+        self.reverse()
+        self.stack()
+        self.combine()
+        self.stack()
+        self.reverse()
+        self.add_new_tile()
+        self.update_gui()
+
+    def up(self, event):
+        self.transpose()
+        self.stack()
+        self.combine()
+        self.stack()
+        self.transpose()
+        self.add_new_tile()
+        self.update_gui()
+
+
+
+    def down(self, event):
+        self.transpose()
+        self.reverse()
+        self.stack()
+        self.combine()
+        self.stack()
+        self.reverse()
+        self.transpose()
+        self.add_new_tile()
+        self.update_gui()
+
+    def hort_move(self):
+        for i in range(4):
+            for j in range(3):
+                if self.martix[i][j] == self.matrix[i][j + 1]:
+                    return True
+        return False
+
+    def game_over(self):
+        if any(2048 in row for row in self.matrix):
+            game_over_frame = tk.Frame(self.main_grid, borderwidth=2)
+            game_over_frame.place(relx=0.5, rely=0.5, anchor='center')
+            tk.Label(
+                text="You Win!",
+                bg=c.winner_bg,
+                fg=c.game_over_font_color,
+                font=c.game_over_font,
+            ).pack()
+        elif not any(0 in row for row in self.matrix)
 
 Game()
